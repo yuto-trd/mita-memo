19865e760df21bb01004b9fe7c852c57fa549dd9
Yuto Terada <lugongdao07@gmail.com>
2024-05-19 00:32:54 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default async function HomePage() {
             <div className="relative" style={{ height: "500px" }}>
                 <img src="/grainy-gradient.svg" className="w-full object-cover absolute mix-blend-color-burn" style={{ height: "500px" }} />
                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <Heading as='h1' color="#082432" size='3xl'>Mita-memo</Heading>
+                    <Heading as='h1' className="text-center" color="#082432" size='3xl'>Mita-memo</Heading>
                     <Text className="mt-3 text-center" fontSize='2xl'>見た作品を記録しよう</Text>
 
                     <div className="flex flex-row gap-4">
